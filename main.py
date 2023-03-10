413453d2edbd7897a3c5977cb9c81814c2aef3be
alex-90314 <119699715+alex-90314@users.noreply.github.com>
2023-03-10 13:12:21 -0500

    merging code from other branches

---
+++ main.py
@@ -0,0 +1,85 @@
+import cv2, os, pytesseract
+
+if os.path.exists("opencv_frame_0.png"):
+    os.remove("opencv_frame_0.png")
+#--------------------------------------------------------------------------------
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    time_elapse = time.time()
+    print(time_elapse)
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        break
+
+
+cam.release()
+
+cv2.destroyAllWindows()
+#--------------------------------------------------------------------------------
+pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread(f"{img_name}")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+
+dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+
+# Finding contours
+contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+ 
+# Creating a copy of image
+im2 = img.copy()
+ 
+# A text file is created and flushed
+file = open("recognized.txt", "w+")
+file.write("")
+file.close()
+
+# Looping through the identified contours
+# Then rectangular part is cropped and passed on
+# to pytesseract for extracting text from it
+# Extracted text is then written into the text file
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+     
+    # Drawing a rectangle on copied image
+    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+     
+    # Cropping the text block for giving input to OCR
+    cropped = im2[y:y + h, x:x + w]
+     
+    # Open the file in append mode
+    file = open("recognized.txt", "a")
+     
+    # Apply OCR on the cropped image
+    text = pytesseract.image_to_string(cropped)
+     
+    # Appending the text into file
+    file.write(text)
+    file.write("\n")
+     
+    # Close the file
+    file.close
